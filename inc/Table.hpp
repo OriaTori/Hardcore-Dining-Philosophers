146dc8b4f9d7384de62e768a0e881c2e8b68fb25
Jakub Zych <jzych89@gmail.com>
2020-03-09 15:31:54 +0100

    Add Log definition

--- inc/Table.hpp
+++ inc/Table.hpp
@@ -3,12 +3,14 @@
 #include <vector>
 #include "Philosopher.hpp"
 #include "Fork.hpp"
+#include "Log.hpp"
 
 struct Table {
+    Log log_;
     std::vector<Fork> forks;
     std::vector<Philosopher> pilosophers;
 
-    Table();
+    Table(Log& log);
     ~Table();
 
     void registerPhilosopher(Philosopher& p);
